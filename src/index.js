b025e03df5ac6e73f7fd510c5a59ba678d96e6a4
Zahan Malkani <zahanm@gmail.com>
2015-08-10 18:24:22 -0700

    rescuing all

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ var fs = require('fs');
 var path = require('path');
 
 var NUM_CPUS = require('os').cpus().length;
+var HIDDEN_FILE_RE = /^\./;
+var APERTURE_FILE_EXT_RE = /\.(apfile|apmaster|apversion)$/i;
+var DONT_CARE_RE = /\.(xml|plist)$/i;
+var APERTURE_FOLDERS = new Set(['Previews', 'Thumbnails']);
 
 class Extractor {
 
@@ -13,20 +17,8 @@ class Extractor {
     this.base = base;
     assert(path.extname(base) === '.aplibrary', 'use with library');
     this.dest = dest;
-    this.dirs = []
-  }
-
-  process(dir) {
-    let contents = fs.readdirSync(dir);
-    contents = contents.map(function(item) { return path.join(dir, item); });
-    for (let item of contents) {
-      let stats = fs.statSync(item);
-      if (stats.isDirectory()) {
-        this.dirs.push(item);
-      } else {
-        console.log('cp %s to %s', item, this.dest);
-      }
-    }
+    this.dirs = [];
+    this.soulsaved = 0;
   }
 
   run() {
@@ -44,11 +36,57 @@ class Extractor {
     console.timeEnd(process.argv[1]);
   }
 
+  process(dir) {
+    let contents = fs.readdirSync(dir);
+    contents = contents.map(item => path.join(dir, item));
+    for (let item of contents) {
+      let stats = fs.statSync(item);
+      if (stats.isDirectory()) {
+        if (this.isWorthTraversing(item)) {
+          this.dirs.push(item);
+        }
+      } else if (this.isWorthSaving(item)) {
+        this.rescue(item);
+      }
+    }
+  }
+
+  rescue(item) {
+    let ext = path.extname(item);
+    this.soulsaved++;
+    let destitem = path.join(this.dest, this.soulsaved + ext)
+    console.log('cp %s to %s', item, destitem);
+    // fs.createReadStream(item).pipe(fs.createWriteStream(destitem));
+  }
+
+  isWorthTraversing(dirpath) {
+    let dname = path.basename(dirpath);
+    if (APERTURE_FOLDERS.has(dname)) {
+      return false;
+    }
+    return true;
+  }
+
+  isWorthSaving(filepath) {
+    let fname = path.basename(filepath);
+    if (HIDDEN_FILE_RE.exec(fname)) {
+      return false;
+    } else if (APERTURE_FILE_EXT_RE.exec(path.extname(fname))) {
+      return false;
+    } else if (DONT_CARE_RE.exec(path.extname(fname))) {
+      return false;
+    }
+    return true;
+  }
+
 }
 
 if (require.main === module) {
   if (process.argv.length < 4) {
-    console.error('usage: %s <src library> <dst dir>', process.argv[1]);
+    console.error(
+      'usage: %s <src library> <dst dir>',
+      path.basename(process.argv[1])
+    );
     process.exit(1);
   }
   let ee = new Extractor(process.argv[2], process.argv[3]);
